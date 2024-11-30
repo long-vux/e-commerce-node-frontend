b63f770e614e8e3426ffa08dbeb5db9d26dfb716
le-dat <hoanglongvu233@gmail.com>
2024-11-30 13:09:38 +0700

    update Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -24,7 +24,7 @@ const Signup = () => {
     }
   }, [user, navigate])
 
-  const handleLoginSuccess = async (response) => {
+  const handleGoogleLoginSuccess = async (response) => {
     const token = response.credential
     try {
       const response = await axios.post(apiUrl + 'api/auth/googleLogin', { token })
@@ -39,82 +39,74 @@ const Signup = () => {
     }
   };
 
-  const handleLoginFailure = (error) => {
+  const handleGoogleLoginFailure = (error) => {
     console.log(error)
     toast.error('Login failed')
   };
 
   const handleSubmit = async e => {
     e.preventDefault()
-
+    if (password !== confirmPassword) {
+      setError('Passwords do not match')
+      return
+    } else if (password.length < 8) {
+      setError('Password must be at least 8 characters long')
+      return
+    } else if (!password.match(/\d/)) {
+      setError('Password must contain at least 1 digit')
+      return
+    } else {
+      setError('')
+    }
     try {
       const response = await axios.post(
-        apiUrl + 'api/account/register',
-        { firstName, lastName, email, phoneNumber, password, confirmPassword },
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
+        apiUrl + 'api/auth/register',
+        { firstName, lastName, email, phoneNumber, password }
       )
-
       if (response.status === 200) {
-        toast.success('Signup successful')
-        navigate('/login')
+        toast.success('Account created successfully, please verify your email')
+        navigate(`/login`)
       }
     } catch (error) {
-      console.error('Error:', error.response)
-      if (error.response.data !== '')
-        setError(error.response.data)
-      else if (error.response.status === 401) {
-        setError(error.response.data)
-      } else if (error.response.status === 400) {
-        setError(
-          error.response.data.errors.FirstName?.[0] ||
-          error.response.data.errors.LastName?.[0] ||
-          error.response.data.errors.Email?.[0] ||
-          error.response.data.errors.PhoneNumber?.[0] ||
-          error.response.data.errors.Password?.[0] ||
-          error.response.data.errors.ConfirmPassword?.[0]
-        )
-      } else if (error.response.status === 500) {
-        setError(error.response.data[0].description)
-      }
+      toast.error(error.response.data.message)
     }
   }
 
   return (
     <div className='flex flex-row justify-around gap-20 items-center h-[600px]'>
       <div className='w-3/5 flex flex-col items-center'>
-        <div className='flex flex-col p-4 rounded-lg ml-20 border border-1 border-gray-300 mb-2 w-[70%]'>
+        <form
+          onSubmit={handleSubmit}
+          className='flex flex-col p-4 rounded-lg ml-20 border border-1 border-gray-300 mb-2 w-[70%]'
+        >
           <h1 className='text-2xl font-bold mb-4'>Sign up</h1>
           <div className='flex flex-row gap-4'>
             <div className='flex flex-col mb-4 w-1/2'>
               <label className='text-sm font-bold mb-1'>First Name</label>
-              <input type="text" value={firstName} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setFirstName(e.target.value)} />
+              <input type="text" value={firstName} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setFirstName(e.target.value)} />
             </div>
             <div className='flex flex-col mb-4 w-1/2'>
               <label className='text-sm font-bold mb-1'>Last Name</label>
-              <input type="text" value={lastName} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setLastName(e.target.value)} />
+              <input type="text" value={lastName} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setLastName(e.target.value)} />
             </div>
           </div>
           <div className='flex flex-col mb-4'>
             <label className='text-sm font-bold mb-1'>Email</label>
-            <input type="email" value={email} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setEmail(e.target.value)} />
+            <input type="email" value={email} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setEmail(e.target.value)} />
           </div>
 
           <div className='flex flex-col mb-4' >
             <label className='text-sm font-bold mb-1'>Phone Number</label>
-            <input type="text" value={phoneNumber} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setPhoneNumber(e.target.value)} />
+            <input type="tel" value={phoneNumber} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setPhoneNumber(e.target.value)} />
           </div>
           <div className='flex flex-row gap-4'>
             <div className='flex flex-col mb-4 w-1/2'>
               <label className='text-sm font-bold mb-1'>Password</label>
-              <input type="password" value={password} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setPassword(e.target.value)} />
+              <input type="password" value={password} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setPassword(e.target.value)} />
             </div>
             <div className='flex flex-col mb-4 w-1/2'>
               <label className='text-sm font-bold mb-1'>Confirm Password</label>
-              <input type="password" value={confirmPassword} className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setConfirmPassword(e.target.value)} />
+              <input type="password" value={confirmPassword} required className='border border-1 border-gray-300 rounded-md p-2' onChange={e => setConfirmPassword(e.target.value)} />
             </div>
           </div>
           {error && (
@@ -122,12 +114,12 @@ const Signup = () => {
               <p className='text-red-500'>{error}</p>
             </div>
           )}
-          <button className='bg-[#1D4567] text-white p-2 mb-4 rounded-md hover:bg-white hover:text-[#1D4567] hover:outline hover:outline-1 hover:outline-[#1D4567] hover:font-bold' onClick={handleSubmit}><span className='font-bold'>Sign up</span></button>
+          <button className='bg-[#1D4567] text-white p-2 mb-4 rounded-md hover:bg-white hover:text-[#1D4567] hover:outline hover:outline-1 hover:outline-[#1D4567] hover:font-bold'><span className='font-bold'>Sign up</span></button>
           <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onFailure={handleLoginFailure}
+            onSuccess={handleGoogleLoginSuccess}
+            onFailure={handleGoogleLoginFailure}
           />
-        </div>
+        </form>
         <span className='flex justify-center text-sm gap-2'>Already have an account?  <a href="/login" className='font-bold underline'>Login</a></span>
       </div>
       <div className='w-2/5'>
